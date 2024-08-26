04d821573de45e9884e70e6ffc009cb65380af4e
Sagarrm <sagarrmhetre@gmail.com>
2024-08-26 21:58:39 +0530

    Project fo rpractivitng jenkins

---
+++ src/main/java/application/CalcMainClass.java
@@ -0,0 +1,16 @@
+package application;
+
+public class CalcMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Main method.");
+		CalcMainClass obj = new CalcMainClass();
+		obj.addition();
+		System.out.println("End of main method.");
+	}
+
+	public void addition() {
+		System.out.println("Inside addition method");
+	}
+}
